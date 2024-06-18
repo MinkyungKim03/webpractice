6031ad9f644d9b1956621419be35d1fcf1bc4898
MinkyungKim03 <162525444+MinkyungKim03@users.noreply.github.com>
2024-06-19 00:28:44 +0900

    Update variable.js

--- variable.js
+++ variable.js
@@ -1,3 +1,12 @@
 var msg = 'Hello';
 msg = 'World';
 console.log(msg);
+
+var msg = 'Hello';
+msg = 100; //값을 변경
+console.log(msg);
+
+var msg = 'Hello';
+var count = 100;
+msg = count;
+console.log(msg);
